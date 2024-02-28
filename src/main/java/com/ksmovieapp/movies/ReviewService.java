dd84adf5ed08e5eea31ccdab6523205d9083963e
kathrynspeltz <kathrynspeltz@gmail.com>
2024-02-28 13:27:23 -0600

    started reviews, added to movies

---
+++ src/main/java/com/ksmovieapp/movies/ReviewService.java
@@ -0,0 +1,10 @@
+package com.ksmovieapp.movies;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public Review createReview(String reviewBody, String imdbId) {
+
+    }
+}
